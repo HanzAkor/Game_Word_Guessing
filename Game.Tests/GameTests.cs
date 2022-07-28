d8617b282533cd91c333e5d7b39f2429deea579e
HanzAkor <95639001+HanzAkor@users.noreply.github.com>
2022-07-28 15:11:18 +0100

    Random tests pass

--- Game.Tests/GameTests.cs
+++ Game.Tests/GameTests.cs
@@ -1,6 +1,7 @@
 using NUnit.Framework;
 using Game;
 using System.Text;
+using NSubstitute;
 
 namespace Game.Tests {
   [TestFixture]
@@ -18,9 +19,17 @@ namespace Game.Tests {
     }
 
     [Test]
-    public void Game_GetRandomWordFromDictionary_ReturnsRandomWordFromDictionary() {
-      Game game = new Game();
-      Assert.AreEqual("MAKERS", game.GetRandomWordFromDictionary());
+    [TestCase(0, "MAKERS")]
+    [TestCase(1, "CANDIES")]
+    [TestCase(2, "DEVELOPER")]
+    [TestCase(3, "LONDON")]
+    public void Game_GetRandomWordFromDictionary_ReturnsRandomWordFromDictionary(int index, string word) {
+      Random fake_rand = Substitute.For<Random>();
+      fake_rand.Next(4).Returns(index);
+      Game game = new Game(fake_rand);
+      string result = game.GetRandomWordFromDictionary();
+      Console.WriteLine(result);
+      Assert.AreEqual(word, result);
     }
   }
 }
